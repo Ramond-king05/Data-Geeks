10413bce3db226bca52bc071949941101ec1b810
Fasasi Abdulrahman Temitope <106965053+Ramond-king05@users.noreply.github.com>
2023-05-07 13:52:45 +0100

    Create app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from streamlit.components.v1 import iframe
 import pandas as pd 
 #import neattext as nt
 import random
+import numpy
 
 
 # Data Viz Pkgs
@@ -33,16 +34,16 @@ st.title("Bible App")
 menu = ["Home","MultiVerse","About"]
 df = load_bible("data/KJV.csv")
 
-co = cohere.Client(cohere_key) # This is your trial API key
-response = co.generate(
-  model='command-xlarge-nightly',
-  prompt='Write a body paragraph about \"My promotion to the head of HR in my company\" in a blog post titled \"My 2023 promotion\"',
-  max_tokens=300,
-  temperature=0.9,
-  k=0,
-  stop_sequences=[],
-  return_likelihoods='NONE')
-print('Prediction: {}'.format(response.generations[0].text))
+co = cohere.Client(cohere_key)  # This is your trial API key
+
+response = co.embed(
+    model='large',
+    texts=[" \"I like Football\"", "I like computer science and AI ", "I studied political science "])
+# print('Embeddings: {}'.format(response.embeddings))
+np_embedding = np.array(response.embeddings)
+print(np_embedding.shape)
+# np.save("co_embedding",np_embedding)
+
 
 
 
